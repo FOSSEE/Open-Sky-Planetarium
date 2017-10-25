09c89f6242ecf2495f86140a3f53ef4678f8563b
SudhakarKuma <sm221294@gmail.com>
2017-10-25 15:53:45 +0530

    Updated OSP-plugin

---
+++ builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/src/Calibrate.cpp" "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/Calibrate.cpp.o"
+  "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/src/LaserDev.cpp" "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/LaserDev.cpp.o"
+  "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/OpenSkyPlanetarium-static_automoc.cpp" "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/OpenSkyPlanetarium-static_automoc.cpp.o"
+  "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/src/OpenSkyPlanetarium.cpp" "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/OpenSkyPlanetarium.cpp.o"
+  "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/src/SerialCom.cpp" "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/SerialCom.cpp.o"
+  "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/src/gui/OSPMainDialog.cpp" "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/gui/OSPMainDialog.cpp.o"
+  "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/qrc_OpenSkyPlanetarium.cpp" "/home/fossee/Downloads/stellarium-0.15.2/plugins/OSP-plugin-master/builds/unix/src/CMakeFiles/OpenSkyPlanetarium-static.dir/qrc_OpenSkyPlanetarium.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_MULTIMEDIA_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../../src"
+  "../../src/gui"
+  "/home/fossee/Downloads/stellarium-0.15.2/src"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/core"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/core/modules"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/core/planetsephems"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/core/external"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/core/external/qtcompress"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/core/external/glues_stel/source"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/core/external/glues_stel/source/libtess"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/gui"
+  "/home/fossee/Downloads/stellarium-0.15.2/src/scripting"
+  "src"
+  "../../src/."
+  "/home/fossee/Qt5.7.0/5.7/gcc_64/include"
+  "/home/fossee/Qt5.7.0/5.7/gcc_64/include/QtCore"
+  "/home/fossee/Qt5.7.0/5.7/gcc_64/./mkspecs/linux-g++"
+  "/home/fossee/Qt5.7.0/5.7/gcc_64/include/QtGui"
+  "/home/fossee/Qt5.7.0/5.7/gcc_64/include/QtWidgets"
+  "/home/fossee/Qt5.7.0/5.7/gcc_64/include/QtMultimedia"
+  "/home/fossee/Qt5.7.0/5.7/gcc_64/include/QtNetwork"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
